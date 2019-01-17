04230f9f6e0a5ac81d99d3ea7beee89d31b685f4
Nikita <mikita1234qwer@gmail.com>
2019-01-17 23:18:48 +0200

    fix routes

--- start/routes.js
+++ start/routes.js
@@ -14,11 +14,12 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
-Route.get('/', () => ({ status: 'Ok', version: '1.0.0' }));
+Route.get('/', () => ({ status: 'OK', version: '1.0.0' }));
 
 Route.group(() => {
-  Route.resource('/login', 'LoginController').apiOnly();
-  Route.resource('/products', 'ProductsController').apiOnly();
-  Route.resource('/products/types', 'ProductsTypesController').apiOnly();
-  Route.resource('/types', 'TypesController').apiOnly();
+  Route.post('/login', 'LoginController.index');
+  Route.post('/registration', 'LoginController.registration');
+  Route.resource('/products', 'ProductsController');
+  Route.resource('/products/:attr/attributes', 'ProductAttributeController');
+  Route.resource('/types', 'TypesController');
 }).prefix('api');
